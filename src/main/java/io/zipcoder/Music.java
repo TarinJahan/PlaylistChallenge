806dc6f40b1e2e6b4f16c8b7d431cdc6b10f58fb
Tarin <tarin@Zipcoders-MacBook-Pro-5.local>
2022-11-30 16:17:05 -0500

    worked on finishing up lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,26 @@
 package io.zipcoder;
 
+import java.util.TreeSet;
+
 public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int currentSong = 0;
+        for (int i = startIndex + 1; i < playList.length; i++) {
+            if (playList[i].equals(selection)) {
+                currentSong = i;
+            }
+        }
+
+        int up = currentSong - startIndex;
+        int down = (startIndex + playList.length) - currentSong;
+
+        return Math.min(up, down);
     }
 }
